742738931d7a57f33ea50b3ee5c9477bfe05359f
Urbano Ballesteros <urbano@cbta193.edu.mx>
2020-12-11 15:06:10 -0600

    init project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LogginComponent } from './auth/loggin/loggin.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { dashboardRoutes } from './dashboard/dashboard.routes';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LogginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: '',
+    component: DashboardComponent ,
+    children: dashboardRoutes
+  },
+  { path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
